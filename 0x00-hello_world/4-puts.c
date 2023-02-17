8b6c51f985b8c160f06f5f29beb1e02189d5356a
naomi <naomi.tsingo@yahoo.com>
2023-02-16 16:20:52 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/**
- * main - main block
- * return: 0
+/*
+ * main - print a lie of code using puts
+ *
+ * Return:0
  */
 int main(void)
-{
-		puts("/"Programming is liked develop a multilingual puzzle");
-			return (0);
+{       
+        puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
 }
