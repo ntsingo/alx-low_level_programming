f32f2e072f92b498ba4e6c43cbb3bd4ce5ec7dae
naume <naomi.tsingo@yahoo.com>
2023-04-20 03:28:33 -0700

    o-read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,54 +1,51 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 
 /**
-
-read_textfile - reads a text file and prints the letters
-
-@filename: filename.
-
-@letters: number of letters to print.
-
-Return: numbers of letters printed. If it fails, returns 0.
-*/
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename
+ * @letters: numbers of letters printed
+ *
+ * Return: numbers of letters printed. If it fails, returns 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd;
-ssize_t nrd, nwr;
-char *buf;
+        int fd;
+        ssize_t nrd, nwr;
+        char *buf;
 
-if (filename == NULL) {
-return 0;
+        if (!filename)
+                return (0);
+
+        fd = open(filename, O_RDONLY);
+        if (fd == -1)
+                return (0);
+
+        buf = malloc(sizeof(char) * letters);
+        if (!buf) {
+                close(fd);
+                return (0);
+        }
+
+        nrd = read(fd, buf, letters);
+        if (nrd == -1) {
+                free(buf);
+                close(fd);
+                return (0);
+        }
+
+        nwr = write(STDOUT_FILENO, buf, nrd);
+        if (nwr == -1) {
+                free(buf);
+                close(fd);
+                return (0);
+        }
+
+        free(buf);
+        close(fd);
+
+        return (nwr);
 }
 
-fd = open(filename, O_RDONLY);
-if (fd == -1) {
-return 0;
-}
-
-buf = malloc(sizeof(char) * letters);
-if (buf == NULL) {
-close(fd);
-return 0;
-}
-
-nrd = read(fd, buf, letters);
-if (nrd == -1) {
-close(fd);
-free(buf);
-return 0;
-}
-
-nwr = write(STDOUT_FILENO, buf, nrd);
-if (nwr == -1) {
-close(fd);
-free(buf);
-return 0;
-}
-
-close(fd);
-free(buf);
-
-return nwr;
-}
