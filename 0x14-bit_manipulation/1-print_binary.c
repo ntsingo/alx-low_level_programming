d08b522c838b3b2d04b89633b135243c7c128623
naume <naomi.tsingo@yahoo.com>
2023-04-20 02:31:37 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,30 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * print_binary
- * @n: decimal number to print
- */
-
 void print_binary(unsigned long int n)
 {
-	unsigned long int temp;
-	int shifts;
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int started = 0;
 
-	if (n == 0)
+	while (mask > 0)
 	{
-		printf("0");
-		return;
+		if ((n & mask) || started)
+		{
+			_putchar((n & mask) ? '1' : '0');
+			started = 1;
+		}
+		mask >>= 1;
 	}
 
-	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
-		;
-
-	for (; shifts >= 0; shifts--)
-	{
-		if ((n >> shifts) & 1)
-			printf("1");
-		else
-			printf("0");
-	}
+	if (!started)
+		_putchar('0');
 }
